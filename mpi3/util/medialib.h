0cb10b25db0fb2b9b594a946c5a8093725ab5d62
matt-blodgett <mattblodgett94@gmail.com>
2018-06-25 15:27:05 -0400

    Library update

--- mpi3/util/medialib.h
+++ mpi3/util/medialib.h
@@ -32,7 +32,27 @@ private:
 class Playlist : public Mpi3Element
 {
 
-}
+private:
+    std::string folder;
+    std::vector<std::string> songs;
 
+};
+
+
+class Song : public Mpi3Element
+{
+
+private:
+    std::string artist;
+    std::string album;
+    std::string time;
+    std::string path;
+    std::string kind;
+
+    int size;
+    int sampleRate;
+    int bitRate;
+
+};
 
 #endif // MEDIALIB_H
