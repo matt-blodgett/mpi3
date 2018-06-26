15c48bb20934da27e24480dd51f45f21a449b169
Matthew <mattblodgett94@gmail.com>
2018-06-25 22:26:56 -0400

    Library util update

--- mpi3/util/medialib.h
+++ mpi3/util/medialib.h
@@ -11,7 +11,12 @@ std::string generatePID();
 class Mpi3Element
 {
 
-private:
+//public:
+//    std::string pid() const;
+//    std::string name() const;
+//    std::string added() const;
+
+public:
     std::string pid = NULL;
     std::string name = NULL;
     std::string added = NULL;
@@ -19,27 +24,7 @@ private:
 };
 
 
-class Folder : public Mpi3Element
-{
-
-private:
-    std::string folder;
-    std::vector<std::string> playlists;
-
-};
-
-
-class Playlist : public Mpi3Element
-{
-
-private:
-    std::string folder;
-    std::vector<std::string> songs;
-
-};
-
-
-class Song : public Mpi3Element
+class Mpi3Song : public Mpi3Element
 {
 
 private:
@@ -54,5 +39,36 @@ private:
     int bitRate;
 
 };
+
+
+class Mpi3Playlist : public Mpi3Element
+{
+
+private:
+    std::string folder;
+    std::vector<std::string> songs;
+
+};
+
+
+class Mpi3Folder : public Mpi3Element
+{
+
+private:
+    std::string folder;
+    std::vector<std::string> playlists;
+
+};
+
+
+class Mpi3Library : public Mpi3Element
+{
+
+private:
+    std::string filepath;
+
+};
+
+
 
 #endif // MEDIALIB_H
