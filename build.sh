144b4d86b53cf1f478c4c6bd97a6ad251bbe3ddc
Matthew <mattblodgett94@gmail.com>
2019-02-21 00:25:16 -0500

    Add configure script for build.sh

--- build.sh
+++ build.sh
@@ -1,121 +1,48 @@
 #!/usr/bin/env bash
 
 
-function print_usage() {
-    cat <<EOF
-Usage: build.sh [device|desktop] [OPTIONS]
-Build script for the mpi3 project
-
-Options:
-   | --help     display this message and exit
- -b| --mode     build mode [debug,release]
- -d| --dest     destination directory
- -h| --host     device host to deploy to [host@address]
- -s| --spec     qmakespec value for qmake
- -q| --qmake    path to qmake command
- -m| --make     path to make command
- -j| --jobs     make jobs
- -w| --warn     use warn_on in qmake config
-
-Git: <https://github.com/matt-blodgett/mpi3.git>
-EOF
-
-    if [[ ! "$1" ]]; then exit 1; else exit "$1"; fi
+function show_message() {
+    echo -ne "\e[1m\e[36m"
+    echo "$1" && echo -ne "\e[0m"
 }
 
 
-function print_error() {
-    echo -ne "\e[1m"
-    echo -ne "\e[31m"
-    echo "$1"
-    echo -ne "\e[0m"
+function show_error() {
+    echo -ne "\e[1m\e[31m"
+    echo "$1" && echo -ne "\e[0m"
+    if [[ "$2" ]]; then exit "$2"; else exit 1; fi
 }
 
 
-function print_message() {
-    echo -ne "\e[1m"
-    echo -ne "\e[36m"
-    echo "$1"
-    echo -ne "\e[0m"
+function check_error() {
+    if [[ "$?" != 0 ]]; then
+        show_error "the program exited with exit code $?" "$?"
+    fi
 }
 
 
-if [[ "$#" == 0 ]]; then print_usage 1; fi
-
-
-BUILD_TARGET="$1"
-BUILD_MODE=
-BUILD_DEST=
-BUILD_HOST=
-BUILD_SPEC=
-QMAKE_CMD=
-MAKE_CMD="/usr/bin/make"
-MAKE_JOBS="$(nproc --all)"
-QMAKE_WARN="warn_off"
-
-
-FLAGS_SHORT="b:d:h:s:q:m:j:w"
-FLAGS_LONG="help,mode:,dest:,host:,spec:,qmake:,make:,jobs:,warn"
-FLAGS=$(getopt -o "$FLAGS_SHORT" --longoptions "$FLAGS_LONG" -- "$@")
-if [[ "$?" != 0 ]]; then exit "$?"; fi
-
-eval set -- "$FLAGS"
-while [[ $# -gt 0 ]]; do
-    case "$1" in
-           --help  ) print_usage 0 ;;
-        -b|--mode  ) BUILD_MODE="$2"; shift ;;
-        -d|--dest  ) BUILD_DEST="$2"; shift ;;
-        -h|--host  ) BUILD_HOST="$2"; shift ;;
-        -s|--spec  ) BUILD_SPEC="$2"; shift ;;
-        -q|--qmake ) QMAKE_CMD="$2"; shift ;;
-        -m|--make  ) MAKE_CMD="$2"; shift ;;
-        -j|--jobs  ) MAKE_JOBS="$2"; shift ;;
-        -w|--warn  ) QMAKE_WARN="warn_on" ;;
-        -- ) break ;;
-        *  ) exit 1 ;;
-    esac
-    shift
-done
-
-
-if [[ ! "$BUILD_TARGET" =~ ^(device|desktop)$ ]]; then
-    print_error "invalid target '$BUILD_TARGET'; valid targets are [device,desktop]"
-    exit 1
-fi
-
-if [[ ! "$BUILD_MODE" =~ ^(debug|release)$ ]]; then
-    print_error "invalid mode '$BUILD_MODE' valid modes are [debug,release]"
-    exit 1
-fi
-
-if [[ ! "$BUILD_SPEC" ]]; then
-    print_error "missing qmakespec value [--spec]"
-    exit 1
-fi
-
-if [[ ! "$QMAKE_CMD" ]]; then
-    print_error "missing qmake command [--qmake]"
-    exit 1
-fi
-
-exit 0
-BUILD_DIR="build-$BUILD_TARGET-$BUILD_MODE"
 PRO_FILE="$PWD/src/build.pro"
 
 
-mkdir "$BUILD_DIR"
-cd "$BUILD_DIR"
+cd build
+check_error "$?"
 
+source config.sh
+check_error "$?"
 
 QMAKE_ARGS=()
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    QMAKE_ARGS+=( "$line" )
+done < config.txt
+check_error "$?"
+
+
+BUILD_DIR="build-$BUILD_TARGET-$BUILD_MODE"
+mkdir "$BUILD_DIR" && cd "$BUILD_DIR"
+
+
 QMAKE_ARGS+=( "DESTDIR=$PWD/mpi3" )
-QMAKE_ARGS+=( "CONFIG+=$QMAKE_WARN" )
 QMAKE_ARGS+=( "CONFIG+=$BUILD_MODE" )
-
-if [[ "$BUILD_MODE" == "debug" ]]; then
-    QMAKE_ARGS+=( "CONFIG+=qml_debug" )
-fi
-
 if [[ "$BUILD_TARGET" == "device" ]]; then
     QMAKE_ARGS+=( "DEFINES+=MPI3_BUILD_DEVICE" )
 elif [[ "$BUILD_TARGET" == "desktop" ]]; then
@@ -123,33 +50,27 @@ elif [[ "$BUILD_TARGET" == "desktop" ]]; then
 fi
 
 
-
 MAKE_ARGS=()
 MAKE_ARGS+=( "-j$MAKE_JOBS" )
-
-if [[ "$BUILD_TARGET" == "device" ]]; then
-    MAKE_ARGS+=( "--ignore-errors" )
-    MAKE_ARGS+=( "--keep-going" )
+if [[ "$BUILD_TARGET" == "device" && "$INSTALL_DEST" ]]; then
+    MAKE_ARGS+=( "--ignore-errors" "--keep-going" )
 fi
 
 
-"$QMAKE_CMD" "$PRO_FILE" -spec "$BUILD_SPEC" "${QMAKE_ARGS[@]}"
+"$BUILD_QMAKE" "$PRO_FILE" -spec "$QMAKE_SPEC" "${QMAKE_ARGS[@]}"
+"$BUILD_MAKE" qmake_all
+"$BUILD_MAKE" -f "$PWD/Makefile" qmake_all
+"$BUILD_MAKE" "${MAKE_ARGS[@]}"
+"$BUILD_MAKE" install
 
-"$MAKE_CMD" qmake_all
-"$MAKE_CMD" -f "$PWD/Makefile" qmake_all
-"$MAKE_CMD" "${MAKE_ARGS[@]}"
-"$MAKE_CMD" install
 
+if [[ "$BUILD_TARGET" == "device" && "$INSTALL_DEST" && "$INSTALL_HOST" ]]; then
+    INSTALL_ARGS=( "-o" "./mpi3device" "main.o" "qrc_fonts.o" "qrc_device.o" )
+    INSTALL_ARGS+=( "libmpi3ui.a" "libmpi3core.a" "libmpi3utils.a" )
+    INSTALL_ARGS+=( "-lao" "-lavcodec" "-lavformat" "-lavutil" )
+    INSTALL_ARGS+=( "-L/usr/local/qt5pi/lib" "-lQt5Widgets" "-lQt5Gui" "-lQt5Xml" "-lQt5Core" )
+    INSTALL_ARGS+=( "-L=/opt/vc/lib" "-lGLESv2" "-lpthread" )
 
-if [[ "$BUILD_TARGET" == "device" && "$BUILD_HOST" && "$BUILD_DEST" ]]; then
-    BUILD_ARGS=()
-    BUILD_ARGS+=( "-o" "./mpi3device" "main.o" "qrc_fonts.o" "qrc_device.o")
-    BUILD_ARGS+=( "-L=/opt/vc/lib" )
-    BUILD_ARGS+=( "libmpi3ui.a" "libmpi3core.a" "libmpi3utils.a" )
-    BUILD_ARGS+=( "-lao" "-lavcodec" "-lavformat" "-lavutil" )
-    BUILD_ARGS+=( "-L/usr/local/qt5pi/lib" "-lQt5Widgets" "-lQt5Gui" "-lQt5Xml" "-lQt5Core" )
-    BUILD_ARGS+=( "-lGLESv2" "-lpthread" )
-
-    scp -r "$PWD" "$BUILD_HOST:$BUILD_DEST"
-    ssh "$BUILD_HOST" "cd $BUILD_DEST/$BUILD_DIR/mpi3 && echo \$PWD && g++ ${BUILD_ARGS[@]} -v"
+    scp -r "$PWD" "$INSTALL_HOST:$INSTALL_DEST"
+    ssh "$INSTALL_HOST" "cd $INSTALL_DEST/$BUILD_DIR/mpi3 && echo \$PWD && g++ ${INSTALL_ARGS[@]} -v"
 fi
---
+++ configure.sh
@@ -0,0 +1,118 @@
+#!/usr/bin/env bash
+
+
+function show_usage() {
+    cat <<EOF
+Usage: configure.sh [OPTIONS]
+Build script for the mpi3 project
+
+Options:
+ --help             display this message and exit
+
+Targets:
+ --desktop          build desktop app
+ --device           build device app
+
+Modes:
+ --release          release build
+ --debug            debug build
+
+Build Variables:
+ --qmake-path       path to qmake
+ --qmake-spec       qmakespec value for qmake
+ --qmake-arg        add additional qmake args
+ --make-path        path to make
+ --make-jobs        make jobs
+
+Install:
+ --install-dest     install destination directory
+ --install-host     install host device [host@address]
+
+Git: <https://github.com/matt-blodgett/mpi3.git>
+EOF
+}
+
+
+function show_error() {
+    echo -ne "\e[1m\e[31m"
+    echo "$1" && echo -ne "\e[0m"
+    if [[ "$2" ]]; then exit "$2"; else exit 1; fi
+}
+
+
+function check_error() {
+    if [[ "$?" != 0 ]]; then
+        show_error "the program exited with exit code $?" "$?"
+    fi
+}
+
+
+if [[ "$#" == 0 ]]; then
+    show_usage && exit 1
+fi
+
+
+BUILD_TARGET=
+BUILD_MODE=
+BUILD_QMAKE=
+QMAKE_SPEC=
+QMAKE_ARGS=()
+BUILD_MAKE=
+MAKE_JOBS=
+INSTALL_DEST=
+INSTALL_HOST=
+
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --help    ) show_usage; exit 0; ;;
+        --desktop ) BUILD_TARGET="desktop"; ;;
+        --device  ) BUILD_TARGET="device"; ;;
+        --debug   ) BUILD_MODE="debug"; ;;
+        --release ) BUILD_MODE="release"; ;;
+        --qmake-path ) BUILD_QMAKE="$2"; shift; ;;
+        --qmake-spec ) QMAKE_SPEC="$2"; shift; ;;
+        --qmake-arg  ) QMAKE_ARGS+=( "$2" ); shift; ;;
+        --make-path  ) BUILD_MAKE="$2"; shift; ;;
+        --make-jobs  ) MAKE_JOBS="$2"; shift; ;;
+        --install-dest ) INSTALL_DEST="$2"; shift; ;;
+        --install-host ) INSTALL_HOST="$2"; shift; ;;
+        * ) show_error "unrecognized flag '$1'"; ;;
+    esac
+    shift
+done
+
+
+if [[ ! "$BUILD_TARGET" ]]; then show_error "missing required build target [--desktop|--device]"; fi
+if [[ ! "$BUILD_MODE" ]]; then show_error "missing required build mode [--release|--debug]"; fi
+if [[ ! "$BUILD_QMAKE" ]]; then show_error "missing required qmake path [--qmake-path]"; fi
+if [[ ! "$QMAKE_SPEC" ]]; then show_error "missing required qmakespec [--qmake-spec]"; fi
+if [[ ! "$BUILD_MAKE" ]]; then show_error "missing required make path [--make-path]"; fi
+if [[ ! "$MAKE_JOBS" ]]; then MAKE_JOBS="$(nproc --all)"; fi
+
+
+if [[ -d build ]]; then rm -rf build; fi
+mkdir build && cd build
+check_error "$?"
+
+
+CONFIG_FILE="config.sh"
+CONFIG_VARS=$(cat <<EOF
+BUILD_TARGET="$BUILD_TARGET"
+BUILD_MODE="$BUILD_MODE"
+BUILD_QMAKE="$BUILD_QMAKE"
+QMAKE_SPEC="$QMAKE_SPEC"
+BUILD_MAKE="$BUILD_MAKE"
+MAKE_JOBS="$MAKE_JOBS"
+INSTALL_DEST="$INSTALL_DEST"
+INSTALL_HOST="$INSTALL_HOST"
+EOF
+)
+
+echo "$CONFIG_VARS" > "$CONFIG_FILE"
+
+
+CONFIG_FILE="config.txt"
+for ARG in "${QMAKE_ARGS[@]}"; do
+    echo "$ARG" >> "$CONFIG_FILE"
+done
